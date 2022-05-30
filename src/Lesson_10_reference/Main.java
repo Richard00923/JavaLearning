a4cce03ba2be4724ff00422db9df6e56b43297fc
Richard00923 <106263087+Richard00923@users.noreply.github.com>
2022-05-30 17:35:39 +0300

    fixed

--- src/Lesson_10_reference/Main.java
+++ src/Lesson_10_reference/Main.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        User max =new User();
+        User max = new User();
         int[] arr = new int[100];
         //System.out.println(Arrays.toString(arr));
     }
 
-    public static void randomMethod(int[] arr) {//-створити метод який отримує пустий масив , і зповнює його рандомними числами. Метод нічтого не повертає!
+    //create a method that gets an empty array, and fills it with random numbers. The method does not return anything!
+    public static void randomMethod(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 10);
         }
     }
 
-    public static void pairMethod(int[] arr) {//-створити метод який отримує пустий масив , і зповнює його парними числами. Метод нічтого не повертає!
-
+    //create a method that gets an empty array and fills it with even numbers. The method does not return anything!
+    public static void pairMethod(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int randomNum = (int) (Math.random() * 100);
             if (randomNum % 2 == 0 && randomNum != 0) {
@@ -30,7 +31,9 @@ public class Main {
             }
         }
     }
-    public static void notEvenMethod(int[] arr) {//-створити метод який отримує пустий масив , і зповнює його непарними числами. Метод нічтого не повертає!
+
+    //create a method that gets an empty array and fills it with odd numbers. The method does not return anything!
+    public static void notEvenMethod(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int randomNum = (int) (Math.random() * 10);
             if (randomNum % 2 != 0) {
@@ -39,12 +42,11 @@ public class Main {
             if (randomNum % 2 == 0) {
                 arr[i] = randomNum + 1;
             }
-            
         }
     }
 
-    public static void nameEditing(User args,String newName) {//-стоврити метод, який приймає об'єкт користувача та змінює йому їм'я на те, яке ви визначите.Метод нічого не повертає!
-    args.name=newName;
+    public static void nameEditing(User args, String newName) {//-стоврити метод, який приймає об'єкт користувача та змінює йому їм'я на те, яке ви визначите.Метод нічого не повертає!
+        args.name = newName;
     }
 }
 
